04da831d31b390aa992a1a59f16831a04bbe7069
ReginaldoPLA <r.pedro.de.lima@avanade.com>
2021-06-02 17:38:27 -0300

    feat: understand about reference

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,6 +6,36 @@ namespace GradeBookTests
 {
   public class TypeTests
   {
+
+    [Fact]
+    public void test1()
+    {
+      var book1 = GetBook("Book1");
+      GetBookSetName(book1, "New Same");
+
+      Assert.Equal("New Same", book1.Name);
+    }
+
+    private void GetBookSetName(Book book, string name)
+    {
+      book = new Book(name);
+      book.Name = name;
+    }
+    [Fact]
+    public void CanSetNameFromReference()
+    {
+      var book1 = GetBook("Book1");
+      SetName(book1, "New Same");
+
+      Assert.Equal("New Same", book1.Name);
+    }
+
+    private void SetName(Book book, string name)
+    {
+      book.Name = name;
+    }
+
+
     [Fact]
     public void GetBookReturnsDifferentObjects()
     {
@@ -14,9 +44,22 @@ namespace GradeBookTests
 
       Assert.Equal("Book1", book1.Name);
       Assert.Equal("Book2", book2.Name);
+
+      Assert.NotSame(book1, book2);
     }
 
-    Book GetBook( string name){
+    public void TwoVarsCanReferenceSameObject()
+    {
+      var book1 = GetBook("Book1");
+      var book2 = book1;
+
+      Assert.Same(book1, book2);
+      Assert.True(Object.ReferenceEquals(book1, book2));
+
+    }
+
+    Book GetBook(string name)
+    {
       return new Book(name);
     }
   }
