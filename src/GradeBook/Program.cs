027a96465c3e23f34f60f6d497440a781fd82ada
ReginaldoPLA <r.pedro.de.lima@avanade.com>
2021-06-07 11:41:14 -0300

    feat: program with try catch and tests

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,9 +10,29 @@ namespace GradeBook
     {
 
       var book = new Book(" Reginaldo Grade Book");
-      book.AddGrade(89.1);
+      /*book.AddGrade(89.1);
       book.AddGrade(90.5);
-      book.AddGrade(77.5);
+      book.AddGrade(77.5);*/
+
+      while (true)
+      {
+        Console.WriteLine("Enter a grade or 'q' to quit");
+        var input = Console.ReadLine();
+        if (input == "q")
+        {
+          break;
+        }
+        try
+        {
+           var grade = double.Parse(input);
+           book.AddGrade(grade);
+        }
+        catch(Exception ex)
+        {
+          Console.WriteLine(ex.Message);
+        }
+
+      }
       var stats = book.GetStatistics();
       Console.WriteLine($"The lowest Grade grade is {stats.Low}!");
       Console.WriteLine($"The High grade is {stats.High}!");
--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -4,8 +4,119 @@ using GradeBook;
 
 namespace GradeBookTests
 {
+  /*
+  // class is reference
+  public class Person {
+
+  }
+
+  //  struct is variable (not reference)
+  public struct Point
+  {
+
+  }
+  */
+
+
   public class TypeTests
   {
+    /*
+      [Fact]
+      public void StringsBehaveLikeValueTypes()
+      {
+        string name = "Scott";
+        MakeUpperCase(name);
+
+        Assert.Equal("SCOTT", name);
+
+      }
+
+      private void MakeUpperCase(string parameter)
+      {
+        parameter.ToUpper();
+      }
+     */
+    [Fact]
+    public void StringsBehaveLikeValueTypes()
+    {
+      string name = "Scott";
+      var upper = MakeUpperCase(name);
+
+      Assert.Equal("SCOTT", upper);
+
+    }
+
+    private string MakeUpperCase(string parameter)
+    {
+      return parameter.ToUpper();
+    }
+
+
+    [Fact]
+
+    public void Test1()
+    {
+      var x = GetInt();
+      SetInt(x);
+      Assert.Equal(3, x);
+    }
+    private void SetInt(int x)
+    {
+      x = 42;
+    }
+
+    private int GetInt()
+    {
+      return 3;
+
+    }
+
+    /*
+    public void Test1() {
+      var x = GetInt();
+      SetInt(ref x);
+
+      Assert.Equal(3,x);
+    }
+    private void SetInt(ref int x)
+    {
+      x = 42;
+    }
+
+    private int GetInt()
+    {
+      return 3;
+
+    }
+    */
+    [Fact]
+    public void CSharpIsPassByRef()
+    {
+      var book1 = GetBook("Book1");
+      GetBookSetName(out book1, "New Same");
+
+      Assert.Equal("New Same", book1.Name);
+    }
+
+    private void GetBookSetName(out Book book, string name)
+    {
+      book = new Book(name);
+    }
+    // out igual ref, exceto que out indica que não foi inicializado
+
+    /* [Fact]
+    public void CSharpIsPassByRef()
+    {
+      var book1 = GetBook("Book1");
+      GetBookSetName(ref book1, "New Same");
+
+      Assert.Equal("New Same", book1.Name);
+    }
+
+    private void GetBookSetName(ref Book book, string name)
+    {
+      book = new Book(name);
+    }*/
 
     [Fact]
     public void CSharpIsPassByValue()
